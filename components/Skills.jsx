ab6a08814f3af3e41a0a2190c35b73d64514a0ae
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-09-06 11:39:22 -0400

    Skills component update

---
+++ components/Skills.jsx
@@ -0,0 +1,106 @@
+import Image from 'next/image';
+import html from '../public/assets/skills/html.png';
+import css from '../public/assets/skills/css.png';
+import javascript from '../public/assets/skills/javascript.png';
+import react from '../public/assets/skills/react.png';
+import next from '../public/assets/skills/nextjs.png';
+import tailwind from '../public/assets/skills/tailwind.png';
+import materialUI from '../public/assets/skills/material-ui.svg';
+import node from '../public/assets/skills/node.png';
+
+const Skills = () => {
+  return (
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] px-10 xl:px-0 mx-auto flex flex-col justify-center h-full">
+        <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={html} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={css} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>CSS</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={javascript} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Javascript</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={react} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>React</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={next} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>NextJS</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={tailwind} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Tailwind</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={materialUI} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>MaterialUI</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 flex justify-center shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 w-full justify-center items-center">
+              <div className="m-auto flex">
+                <Image src={node} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>NodeJS</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import About from '../components/About';
 import Main from '../components/Main';
 import Navbar from '../components/Navbar';
+import Skills from '../components/Skills';
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       </Head>
       <Navbar />
       <Main />
+      <About />
+      <Skills />
     </div>
   );
 }
