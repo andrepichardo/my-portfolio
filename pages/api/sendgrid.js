ce012cb5baa192c566af5e3ce7226d7f06ce7de3
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2023-01-22 19:43:56 -0400

    sendgrid update

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -7,7 +7,7 @@ async function sendEmail(req, res) {
     await sendgrid.send({
       to: 'andrep97@hotmail.com', // Your email where you'll receive emails
       from: 'contact@andrepichardo.com', // your website email address here
-      subject: `[Lead from website] : ${req.body.subject}`,
+      subject: `[Lead from Portfolio] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
       <head>
