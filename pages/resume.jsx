b7c52b8278c259acf1ee55b6f2fce0783985a10c
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2023-02-17 20:27:43 -0400

    Resume update

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -19,14 +19,14 @@ const Resume = () => {
           <h2 className="text-center">André Pichardo</h2>
           <div className="flex">
             <a
-              href="https://www.linkedin.com/in/clint-briley-50056920a/"
+              href="https://www.linkedin.com/in/andre-pichardo/"
               target="_blank"
               rel="noreferrer"
             >
               <FaLinkedinIn size={20} style={{ marginRight: '1rem' }} />
             </a>
             <a
-              href="https://github.com/fireclint"
+              href="https://github.com/andrepichardo"
               target="_blank"
               rel="noreferrer"
             >
@@ -37,26 +37,23 @@ const Resume = () => {
         <div className="text-center py-4 text-xl font-bold uppercase tracking-wider">
           <div className="hidden sm:block">
             <p>
-              Proven Leadership <span className="px-1">|</span> Web Development{' '}
-              <span className="px-1">|</span> Complex Problem Solving
+              Teamwork <span className="px-1">|</span> Web Development{' '}
+              <span className="px-1">|</span>Problem Solving
             </p>
           </div>
           <div className="block sm:hidden">
-            <p>Proven Leadership</p>
+            <p>Teamwork</p>
             <p className="py-2">Web Development</p>
-            <p>Complex Problem Solving</p>
+            <p>Problem Solving</p>
           </div>
         </div>
         <p className="text-justify">
-          Analytical, innovative, and motivated web development professional
-          with experience in customer service, team leadership, and
-          organizational effectiveness in fast-paced and challenging
-          environments. Adept at developing strategies and driving streamlined
-          operations. Diverse analytical skills, team collaboration, and
-          relationship building. Consummate professional, and motivated leader,
-          with solid interpersonal abilities and complex problem-solving skills.
-          Effective and proven track record of critical thinking, idea
-          generation, and optimizing efficiencies.
+          Electronics and Communications Engineer based in Dominican Republic,
+          currently specialized in Frontend Web Development field, constantly
+          learning new technologies to grow in this great industry. Passionate,
+          innovative, and motivated professional with experience in team
+          collaboration, problem solving, and organizational effectiveness in
+          fast-paced and challenging environments.
         </p>
 
         {/* Skills */}
@@ -64,23 +61,26 @@ const Resume = () => {
           <h5 className="text-center underline text-[18px] py-2">Skills</h5>
           <p className="py-2">
             <span className="font-bold">Technical Skills</span>
-            <span className="px-2">|</span>Front-End Web Developer
-            <span className="px-2">|</span> HTML
+            <span className="px-2">|</span>Web Development
+            <span className="px-2">|</span>HTML
             <span className="px-2">|</span>CSS
             <span className="px-2">|</span>Javascript
             <span className="px-2">|</span>React
-            <span className="px-2">|</span>Next JS
-            <span className="px-2">|</span>SQL
-            <span className="px-2">|</span>NoSQL
-            <span className="px-2">|</span>Redux
-            <span className="px-2">|</span>Tailwind
-            <span className="px-2">|</span> Firebase
-            <span className="px-2">|</span> RESTAPI
+            <span className="px-2">|</span>NextJS
+            <span className="px-2">|</span>PostgreSQL
+            <span className="px-2">|</span> Redux
+            <span className="px-2">|</span>TailwindCSS
+            <span className="px-2">|</span>Supabase
+            <span className="px-2">|</span>Firebase
+            <span className="px-2">|</span>RESTAPI
           </p>
           <p className="py-2">
-            <span className="font-bold">Amazon Web Services</span>
-            <span className="px-2">|</span>Amazon Web Services Cloud
-            Practitioner
+            <span className="font-bold">Soft Skills</span>
+            <span className="px-2">|</span>Communication
+            <span className="px-2">|</span>Teamwork
+            <span className="px-2">|</span>Organization
+            <span className="px-2">|</span>Problem Solving
+            <span className="px-2">|</span>Flexibility
           </p>
         </div>
 
@@ -90,53 +90,60 @@ const Resume = () => {
         {/* Experience */}
         <div className="py-6">
           <p className="italic">
-            <span className="font-bold italic">INFINITE TECH</span>
-            <span className="px-2">|</span>Dallas, TX
+            <span className="font-bold italic">
+              Oficina Gubernamental De Tecnologías De La Información Y
+              Comunicación (OGTIC)
+            </span>
+            <span className="px-2">|</span>Distrito Nacional, DO
           </p>
           <p className="py-1 italic">
-            Front End Web Developer & Web Master (2013 - Current)
+            Front End Web Developer (Aug 2021 - Current)
           </p>
           <ul className="list-disc list-outside px-7 py-1 leading-relaxed">
             <li>
-              Ensured comprehensive mobile responsive UI layouts with
-              understanding of HTML, CSS, and JavaScript.
+              Development using mostly React.js, TypeScript & TailwindCSS.
             </li>
             <li>
-              Troubleshoot issues and concerns, configure operation and analytic
-              plugins, and design changes as needed.
+              Create mobile responsive web applications with special attention
+              and implementation of SEO, UI/UX and clean code.
             </li>
             <li>
-              Worked directly with a diverse client base to understand and
-              implement multiple technologies and programs.
+              Troubleshoot issues and concerns found in QA Testing process, such
+              as functionality and design changes as needed.
             </li>
             <li>
-              Platform migration managing DNS servers as well as MX records and
-              ensuring a smooth migration process.
+              Work along with other developers to create and implement multiple
+              technologies and programs.
             </li>
           </ul>
         </div>
 
-        {/* Personal Experience */}
+        {/* Experience */}
         <div className="py-6">
           <p className="italic">
-            <span className="font-bold italic">
-              Teaching & Mentoring Web Development
-            </span>
-            <span className="px-2">|</span>Dallas, TX
+            <span className="font-bold italic">Altice Dominicana</span>
+            <span className="px-2">|</span>Distrito Nacional, DO
           </p>
           <p className="py-1 italic">
-            Code Commerce - Youtube Channel (2021 - Current)
+            Software QA Tester (Jul 2019 - Aug 2021)
           </p>
           <ul className="list-disc list-outside px-7 py-1 leading-relaxed">
             <li>
-              In 2021 I started “Code Commerce”, a YouTube channel focused on
-              sharing what I’m learning in web development, specifically
-              front-end development.
+              Carrying out different QA tests to guarantee the correct operation
+              of the new FW and SW that were deployed throughout the Headend
+              network (TV clients).
             </li>
             <li>
-              Private 1 on 1 Zoom coaching calls related to web development.
-              More specifically debugging, teaching basic web development
-              concepts and code reviews.
+              Identification of errors / bugs to later be reported and corrected
+              with the developer team.
+            </li>
+            <li>
+              Constant communication with foreign developers from Europe (in
+              English).
+            </li>
+            <li>
+              Weekly reports with the results of the tests carried out on the
+              latest version of FW / SW released.
             </li>
           </ul>
         </div>
@@ -149,36 +156,25 @@ const Resume = () => {
         {/* Experience */}
         <div className="py-6">
           <p className="italic">
-            <span className="font-bold">CITY OF SHERMAN, TX</span>
-            <span className="px-2">|</span>Sherman, TX
+            <span className="font-bold">Altice Dominicana</span>
+            <span className="px-2">|</span>Distrito Nacional, DO
           </p>
-          <p className="py-1 italic">Paramedic / Firefighter (2010 – 2019)</p>
+          <p className="py-1 italic">
+            Warehouse Assistant (May 2019 - Jul 2019)
+          </p>
           <ul className="list-disc list-outside px-7 py-1 leading-relaxed">
             <li>
-              Ensured proper implementation of firefighting techniques and
-              practices including controlling and extinguishing fires, operating
-              hose lines, and fire ground operations, ventilation, and search
-              and rescue.
+              Assist with receiving, unloading, counting and stocking physical
+              inventory in the warehouse.
             </li>
             <li>
-              Oversaw comprehensive understanding, operation, and maintenance of
-              fire engines, ladder trucks, technical rescue apparatus, and
-              response trailers.
+              Ensure orders are processed efficiently and that the delivery of
+              materials meets business timelines.
             </li>
+            <li>Pack item according to specified packing guidelines.</li>
             <li>
-              Performed ALS techniques as needed within emergency situations
-              including the use of an esophageal or dual lumen airway device and
-              oral intubation using laryngoscopy.
-            </li>
-            <li>
-              Collaborated with a talented and high performing team of
-              firefighters in high pressure and stressful situations as needed.
-            </li>
-            <li>
-              Utilized proven and demonstrated knowledge of multiple
-              certifications including Pediatric Advanced Life Support,
-              Prehospital Trauma Life Support, and Advanced Cardiac Life
-              Support, among others.
+              Inspect items to ensure they&apos;re not damaged or faulty and
+              adjust inventory accordingly.
             </li>
           </ul>
         </div>
