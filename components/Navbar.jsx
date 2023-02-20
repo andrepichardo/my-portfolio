59564207642fb4e9cdc06bda22cf21f34b794f6b
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2023-02-20 15:56:47 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -164,7 +164,7 @@ const Navbar = () => {
                 </p>
               </div>
             </div>
-            <div className=" flex flex-col justify-between gap-8 h-full">
+            <div className=" flex flex-col justify-between gap-8 mb-10 h-full">
               <ul className="uppercase flex flex-col gap-1">
                 <Link href="/">
                   <li
